e58ef3be81891cba3378cb3a40137ef2cd841bba
SilverClick <113641305+SilverClick@users.noreply.github.com>
2023-05-06 12:00:03 +0200

    Add files via upload

---
+++ src/boletin29/Boletin29.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin29;
+
+public class Boletin29 {
+
+  
+    public static void main(String[] args) {
+       Ventana1 obj=new Ventana1();
+       obj.setVisible(true);
+       Ventana2 obj2=new Ventana2();
+       obj.setVisible(true);
+    }
+    
+}
