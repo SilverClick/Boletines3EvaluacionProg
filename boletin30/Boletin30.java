5fc5ebfea80d5951122bbf72bf22e88ce503a6f9
SilverClick <113641305+SilverClick@users.noreply.github.com>
2023-05-06 12:04:42 +0200

    Add files via upload

---
+++ boletin30/Boletin30.java
@@ -0,0 +1,14 @@
+
+package boletin30;
+
+
+public class Boletin30 {
+
+    
+    public static void main(String[] args) {
+        
+        Ventana obx = new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
